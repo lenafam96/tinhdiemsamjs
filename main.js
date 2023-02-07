0659382a890a112130a96621e22d63221e507f89
Lê Minh <cong.minh.hvt@gmail.com>
2023-02-07 17:29:29 +0700

    update function

--- main.js
+++ main.js
@@ -4,15 +4,22 @@ let table = document.getElementById("table");
 let thead = document.getElementById("thead");
 let tbody = document.getElementById("tbody");
 let tfoot = document.getElementById("tfoot");
+let id_row_clicked = document.getElementById("id-row-clicked");
 
 let arrayPlayers = localStorage.getItem("arrayPlayers")?localStorage.getItem("arrayPlayers").split(","):[];
 
 let arrayRowScore = [];
 
 if(localStorage.length>1){
-    for (let i = 1; i < localStorage.length; i++){
-        let row = JSON.parse(localStorage.getItem(i));
-        arrayRowScore.push(row);
+    for (let i = 0; i < localStorage.length; i++){
+        if(localStorage.key(i)!="arrayPlayers")
+        {
+            let row = {
+                key:localStorage.key(i),
+                value:JSON.parse(localStorage.getItem(localStorage.key(i)))
+            };
+            arrayRowScore.push(row);
+        }
     }
 }
 
@@ -26,10 +33,9 @@ if(arrayPlayers.length>0){
 
 function addPlayer(){
     let player = document.getElementById("name").value;
+    if(player==="" || arrayPlayers.indexOf(player)!=-1) return;
     arrayPlayers.push(player);
     localStorage.setItem("arrayPlayers", arrayPlayers);
-    console.log(localStorage.getItem("arrayPlayers"));
-    console.log(arrayPlayers);
     createPlayer(player);
     if(arrayPlayers.length === 1){
         createButton();
@@ -57,6 +63,7 @@ function createPlayer(name){
 
     let playerScore = document.createElement("input");
     playerScore.type = "number";
+    playerScore.placeholder = 0;
     playerScore.id = name;
     player.appendChild(playerScore);
 
@@ -64,6 +71,35 @@ function createPlayer(name){
     btnDelete.textContent = "X";
     btnDelete.onclick = () => deletePlayer(name);
     player.appendChild(btnDelete);
+
+    let select = document.createElement("select");
+    // select.id = `select-${name}`;
+    select.onchange = (e) => handleSelectionChange(e,name);
+    let option = document.createElement("option");
+    option.value = 0;
+    option.textContent = 'Tuỳ chọn thêm';
+    select.appendChild(option);
+    option = document.createElement("option");
+    option.value = 1;
+    option.textContent = 'Báo sâm';
+    select.appendChild(option);
+    option = document.createElement("option");
+    option.value = 2;
+    option.textContent = 'Chặn sâm';
+    select.appendChild(option);
+    option = document.createElement("option");
+    option.value = 3;
+    option.textContent = 'Bị chặn sâm';
+    select.appendChild(option);
+    option = document.createElement("option");
+    option.value = 4;
+    option.textContent = 'Bắt tứ quý';
+    select.appendChild(option);
+    option = document.createElement("option");
+    option.value = 5;
+    option.textContent = 'Bị bắt tứ quý';
+    select.appendChild(option);
+    player.appendChild(select);
 
     divInput.appendChild(player);
 }
@@ -81,9 +117,11 @@ function createTable() {
     thead.appendChild(trHead);
     arrayRowScore.forEach((item)=>{
         let tr = document.createElement("tr");
+        tr.onclick = ()=>handleRowClick(tr);
+        tr.id = item.key;
         arrayPlayers.forEach((player)=>{
             let td = document.createElement("td");
-            td.innerHTML = item[player];
+            td.innerHTML = item.value[player]??0;
             tr.appendChild(td);
         })
         // console.log(arrayRowScore);
@@ -93,44 +131,166 @@ function createTable() {
 }
 
 function createButton(){
-    let button = document.createElement("button");
-    button.textContent = "Enter";
-    button.onclick = () => enterScore();
-    divButton.appendChild(button);
+    let buttonEnter = document.createElement("button");
+    buttonEnter.textContent = "Enter";
+    buttonEnter.onclick = () => enterScore();
+    divButton.appendChild(buttonEnter);
+    let buttonUpdate = document.createElement("button");
+    buttonUpdate.textContent = "Update";
+    buttonUpdate.id = "btnUpdate";
+    buttonUpdate.onclick = () => updateScore();
+    divButton.appendChild(buttonUpdate);
+    let buttonDelete = document.createElement("button");
+    buttonDelete.textContent = "Delete";
+    buttonDelete.id = "btnDelete";
+    buttonDelete.onclick = () => deleteScore();
+    divButton.appendChild(buttonDelete);
+    enableButton();
+}
+
+function enableButton(){
+    let buttonUpdate = document.getElementById("btnUpdate");
+    let buttonDelete = document.getElementById("btnDelete");
+    if(id_row_clicked.value === ''){
+        buttonUpdate.disabled = true;
+        buttonDelete.disabled = true;
+    }
+    else{
+        buttonUpdate.disabled = false;
+        buttonDelete.disabled = false;
+    }
 }
 
 function enterScore() {
     let input = document.querySelectorAll("input[type='number']");
-    let check = false;
-    // input.forEach(item => {
-    //     if item.value >
-    // })
+    if(checkScore([...input]))
+        return;
     
-    console.log([...input].every(item=>{return item.value > 0}));
-    // let obj = [...input].reduce((acc, curr) => {
-    //     acc[curr.id] = curr.value;
-    //     return acc;
-    // },{})
-    // // console.log(obj);
-    // localStorage.setItem(localStorage.length, JSON.stringify(obj));
-    // console.log(localStorage);
-    // console.log(JSON.parse(localStorage.getItem(1)));
-    // let tr = document.createElement("tr");
-    // arrayPlayers.forEach((player)=>{
-    //     let td = document.createElement("td");
-    //     td.innerHTML = obj[player];
-    //     tr.appendChild(td);
-    // })
-    // // console.log(arrayRowScore);
-    // tbody.appendChild(tr);
-    // input = document.getElementsByTagName('input');
-    // for (let index = 0; index < input.length; index++) {
-    //     input[index].value = '';
-    // }
+    // console.log(input);
+    
+    let obj = [...input].reduce((acc, curr) => {
+        acc[curr.id] = Number(curr.value);
+        return acc;
+    },{})
+    processObject(obj);
+    console.log(obj);
+    let key = Date.now();
+    localStorage.setItem(key, JSON.stringify(obj));
+    let tr = document.createElement("tr");
+    tr.id = key;
+    tr.onclick = ()=>handleRowClick(tr);
+    arrayPlayers.forEach((player)=>{
+        let td = document.createElement("td");
+        td.innerHTML = obj[player];
+        tr.appendChild(td);
+    })
+    // console.log(arrayRowScore);
+    tbody.appendChild(tr);
+    clearInput();
+}
+
+function clearInput(){
+    input = document.getElementsByTagName('input');
+    for (let index = 0; index < input.length; index++) {
+        input[index].value = '';
+    }
+}
+
+function updateScore(){
+    if(id_row_clicked.value === "")
+        return;
+    let input = document.querySelectorAll("input[type='number']");
+    let obj = [...input].reduce((acc, curr) => {
+        acc[curr.id] = Number(curr.value);
+        return acc;
+    },{})
+    localStorage.setItem(id_row_clicked.value,JSON.stringify(obj));
+    let tr = document.getElementById(id_row_clicked.value);
+    removeElement(tr);
+    arrayPlayers.forEach((player)=>{
+        let td = document.createElement("td");
+        td.innerHTML = obj[player];
+        tr.appendChild(td);
+    });
+    clearInput();
+    enableButton();
+}
+
+function deleteScore(){
+    if(id_row_clicked.value === "")
+        return;
+    localStorage.removeItem(id_row_clicked.value);
+    let tr = document.getElementById(id_row_clicked.value);
+    removeElement(tr);
+    clearInput();
+    enableButton();
+}
+
+function checkScore(arrayScore){
+    return (arrayScore.some(item=>{return item.value > 0}) 
+    || countZero(arrayScore) !== 1 
+    )
+}
+
+function countZero(array){
+    let count = 0;
+    for(let i=0; i<array.length;i++){
+        if(array[i].value == 0)
+            count++;
+    }
+    return count;
+}
+
+function processObject(obj){
+    let sum = 0;
+    for(const key in obj){
+        sum += obj[key];
+    }
+    for(const key in obj){
+        if(obj[key] === 0){
+            obj[key] = sum*-1;
+            break;
+        }
+    }
 }
 
 function removeElement(container){
     while (container.firstChild) {
         container.removeChild(container.lastChild);
+    }
+}
+
+function handleRowClick(e){
+    id_row_clicked.value = e.id;
+    let row = JSON.parse(localStorage.getItem(e.id));
+    let inputs = document.getElementsByTagName('input');
+    for (let input of inputs) {
+        if(arrayPlayers.includes(input.id))
+            input.value = row[input.id];
+    }
+    enableButton();
+}
+
+function clearLocalStorage(){
+    if(confirm('Are you sure you want to delete all data?')==true){
+        localStorage.clear();
+        removeElement(divInput);
+        removeElement(divButton);
+        removeElement(thead);
+        removeElement(tbody);
+        removeElement(tfoot);
+    }
+}
+
+function handleSelectionChange(e,name){
+    console.log(e.target.value);
+    let input = document.getElementById(name);
+    switch (e.target.value) {
+        case '1':
+            input.value = 120;
+            break;
+    
+        default:
+            break;
     }
 }
